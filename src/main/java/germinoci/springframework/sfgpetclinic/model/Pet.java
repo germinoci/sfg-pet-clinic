fe09e8aba96a3e1a2a579ec9ab08ee39ef902888
pnemec <petr.nemec@transen.sk>
2020-07-15 15:52:51 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/germinoci/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package germinoci.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType lastName;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(PetType lastName) {
+        this.lastName = lastName;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
---
+++ src/main/java/germinoci/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package germinoci.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
