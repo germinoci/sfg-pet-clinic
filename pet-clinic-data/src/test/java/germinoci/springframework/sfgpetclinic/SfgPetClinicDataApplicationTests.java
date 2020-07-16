73f64808bb065b547f384366b40473ca519fd31c
pnemec <petr.nemec@transen.sk>
2020-07-16 13:12:47 +0200

    refactoring to multimodue amven project. Closes #1.

---
+++ pet-clinic-data/src/test/java/germinoci/springframework/sfgpetclinic/SfgPetClinicDataApplicationTests.java
@@ -0,0 +1,13 @@
+package germinoci.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- pet-clinic-web/src/test/java/germinoci/springframwork/sfgpetclinic/SfgPetClinicWebApplicationTests.java
+++ pet-clinic-web/src/test/java/germinoci/springframwork/sfgpetclinic/SfgPetClinicWebApplicationTests.java
@@ -1,10 +1,10 @@
-package germinoci.springframework.sfgpetclinic;
+package germinoci.springframwork.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SfgPetClinicApplicationTests {
+class SfgPetClinicWebApplicationTests {
 
     @Test
     void contextLoads() {
