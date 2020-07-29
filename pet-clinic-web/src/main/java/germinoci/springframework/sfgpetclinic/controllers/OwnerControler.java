f4e1f03e588facd2a16058f6aaa1f58a3b8cd338
pnemec <petr.nemec@transen.sk>
2020-07-29 09:53:12 +0200

    adding owners index page and controller. Closes #13.

---
+++ pet-clinic-web/src/main/java/germinoci/springframework/sfgpetclinic/controllers/OwnerControler.java
@@ -0,0 +1,15 @@
+package germinoci.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerControler {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listOwners(){
+        return "owners/index";
+    }
+
+}
