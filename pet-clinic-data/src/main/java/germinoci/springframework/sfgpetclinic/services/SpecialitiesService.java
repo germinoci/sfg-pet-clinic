12548968735726e5f680fd4204d73b5536120bbd
pnemec <petr.nemec@transen.sk>
2020-08-06 15:10:23 +0200

    added Specialities Map impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/germinoci/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package germinoci.springframework.sfgpetclinic.services;
+
+import germinoci.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/germinoci/springframework/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,33 @@
+package germinoci.springframework.sfgpetclinic.services.map;
+
+import germinoci.springframework.sfgpetclinic.model.Speciality;
+import germinoci.springframework.sfgpetclinic.services.SpecialitiesService;
+import java.util.Set;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
