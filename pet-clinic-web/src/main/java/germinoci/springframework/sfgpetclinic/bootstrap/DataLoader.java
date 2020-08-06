50daa9c45dfa7c0e9caf53b893eaa4e2933d44ec
pnemec <petr.nemec@transen.sk>
2020-08-06 14:35:45 +0200

    added PetType to bootstrap. Closes #32.

--- pet-clinic-web/src/main/java/germinoci/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/germinoci/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package germinoci.springframework.sfgpetclinic.bootstrap;
 
 import germinoci.springframework.sfgpetclinic.model.Owner;
+import germinoci.springframework.sfgpetclinic.model.PetType;
 import germinoci.springframework.sfgpetclinic.model.Vet;
 import germinoci.springframework.sfgpetclinic.services.OwnerService;
+import germinoci.springframework.sfgpetclinic.services.PetTypeService;
 import germinoci.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,13 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
-
 
 //    public DataLoader() {
 //        ownerService = new OwnerServiceMap();
@@ -26,6 +29,15 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
